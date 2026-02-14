e60fa9f12fb0e5627e6d4b34371ad7f59ae627a1
contactarnobbiswas-oss <contact.arnobbiswas@gmail.com>
2026-02-15 04:46:01 +0600

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,50 +1,79 @@
+import React from 'react'
+
 export default function Footer() {
-    // ইউনিফর্ম সার্কুলার আইকন ব্যবহার করা হয়েছে
-    const socialIcons = [
-        { name: 'Facebook', url: 'https://img.icons8.com/ios-filled/50/facebook-circled--v1.png' },
-        { name: 'Instagram', url: 'https://img.icons8.com/ios-filled/50/instagram-circled--v1.png' },
-        { name: 'Twitter X', url: 'https://img.icons8.com/ios-filled/50/x-circled.png' },
-        { name: 'YouTube', url: 'https://img.icons8.com/ios-filled/50/youtube-circled--v1.png' },
-        { name: 'LinkedIn', url: 'https://img.icons8.com/ios-filled/50/linkedin-circled--v1.png' }
-    ];
+  // সোশ্যাল মিডিয়া আইকনগুলোর জন্য সরাসরি SVG কোড ব্যবহার করা হয়েছে
+  const socialIcons = [
+    { 
+      name: 'Facebook', 
+      link: '#!', 
+      svg: <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+    },
+    { 
+      name: 'Instagram', 
+      link: '#!', 
+      svg: <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.919-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+    },
+    { 
+      name: 'Twitter X', 
+      link: '#!', 
+      svg: <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932L18.901 1.153zm-1.291 19.492h2.039L6.486 3.24H4.298l13.312 17.405z"/>
+    },
+    { 
+      name: 'YouTube', 
+      link: '#!', 
+      svg: <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+    },
+    { 
+      name: 'LinkedIn', 
+      link: '#!', 
+      svg: <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+    }
+  ];
 
-    return (
-        <div className="mt-20">
-            <div className="text-center">
-                <a href="#!">
-                    <img src="/assets/logo.png" alt="Logo" className="w-36 mx-auto mb-2 dark:hidden" />
-                    <img src="/assets/logo_dark.png" alt="Logo Dark" className="w-36 mx-auto mb-2 hidden dark:block" />
-                </a>
-
-                <div className="w-max flex items-center gap-2 mx-auto text-gray-600 dark:text-white/90">
-                    <img src="https://img.icons8.com/ios-filled/50/mail.png" alt="Mail" className="w-5 dark:invert opacity-80" />
-                    <a href="mailto:contact.arnobbiswas@gmail.com">contact.arnobbiswas@gmail.com</a>
-                </div>
-            </div>
-
-            <div className="text-center sm:flex items-center justify-between border-t border-gray-300 dark:border-white/20 mx-[10%] mt-12 py-7">
-                
-                {/* টেক্সট কালার এখন আইকনের সাথে ১০০% ম্যাচ করবে */}
-                <p className="text-gray-600 dark:text-white/80 text-sm">© 2026 Arnob Biswas • All rights reserved.</p>
-
-                <ul className="flex items-center gap-5 justify-center mt-4 sm:mt-0">
-                    {socialIcons.map((icon, index) => (
-                        <li key={index}>
-                            <a target='_blank' href="#!" className="group">
-                                <img 
-                                    src={icon.url} 
-                                    alt={icon.name} 
-                                    /* - opacity-60 (Light): টেক্সট কালারের সাথে মিলবে
-                                      - dark:opacity-90 (Dark): ডার্ক মোডে উজ্জ্বল দেখাবে
-                                      - Circular Icons: সব আইকন এখন ইউনিফর্ম শেপের
-                                    */
-                                    className="w-6 transition-all duration-300 dark:invert opacity-60 dark:opacity-90 group-hover:opacity-100 group-hover:scale-125 group-hover:filter group-hover:drop-shadow-sm" 
-                                />
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        {/* লোগো */}
+        <div className="flex justify-center mb-2">
+            <h3 className="text-3xl font-Ovo dark:text-white">
+                Arnob<span className="text-[#8c0522]">.</span>
+            </h3>
         </div>
-    )
+
+        {/* ইমেইল */}
+        <div className="w-max flex items-center gap-2 mx-auto text-gray-700 dark:text-white/90">
+          <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
+            <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
+          </svg>
+          <a href="mailto:contact.arnobbiswas@gmail.com">contact.arnobbiswas@gmail.com</a>
+        </div>
+      </div>
+
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-7">
+        {/* কপিরাইট */}
+        <p className="text-gray-600 dark:text-white/80">© 2026 Arnob Biswas • All rights reserved.</p>
+
+        {/* সোশ্যাল আইকন (SVG) */}
+        <ul className="flex items-center gap-6 justify-center mt-4 sm:mt-0">
+          {socialIcons.map((item, index) => (
+            <li key={index}>
+              <a 
+                href={item.link} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="text-gray-600 dark:text-white/90 hover:text-[#8c0522] dark:hover:text-[#8c0522] transition-all duration-300"
+              >
+                <svg 
+                  viewBox="0 0 24 24" 
+                  className="w-6 h-6 fill-current hover:scale-125 transition-transform"
+                >
+                  {item.svg}
+                </svg>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
