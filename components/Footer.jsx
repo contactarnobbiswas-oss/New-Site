23a994fc9b42435c6e38640ca02eacd1a7dc7ed8
contactarnobbiswas-oss <contact.arnobbiswas@gmail.com>
2026-02-15 04:18:29 +0600

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,25 @@
 export default function Footer() {
+    // অনলাইন আইকন লিঙ্কগুলো এখানে সেট করা হয়েছে
+    const socialIcons = [
+        { name: 'Facebook', url: 'https://img.icons8.com/ios-filled/50/facebook-new.png' },
+        { name: 'Instagram', url: 'https://img.icons8.com/ios-filled/50/instagram-new.png' },
+        { name: 'Twitter X', url: 'https://img.icons8.com/ios-filled/50/twitterx--v2.png' },
+        { name: 'YouTube', url: 'https://img.icons8.com/ios-filled/50/youtube-play.png' },
+        { name: 'LinkedIn', url: 'https://img.icons8.com/ios-filled/50/linkedin.png' }
+    ];
+
     return (
         <div className="mt-20">
             <div className="text-center">
                 {/* লোগো সেকশন */}
                 <a href="#!">
-                    <img src="/assets/logo.png" alt="" className="w-36 mx-auto mb-2 dark:hidden" />
-                    <img src="/assets/logo_dark.png" alt="" className="w-36 mx-auto mb-2 hidden dark:block" />
+                    <img src="/assets/logo.png" alt="Logo" className="w-36 mx-auto mb-2 dark:hidden" />
+                    <img src="/assets/logo_dark.png" alt="Logo Dark" className="w-36 mx-auto mb-2 hidden dark:block" />
                 </a>
 
-                {/* ইমেইল সেকশন - আপনার দেওয়া ইমেইল আপডেট করা হয়েছে */}
-                <div className="w-max flex items-center gap-2 mx-auto">
-                    <img src="/assets/mail_icon.png" alt="" className="w-5 dark:hidden" />
-                    <img src="/assets/mail_icon_dark.png" alt="" className="w-5 hidden dark:block" />
+                {/* ইমেইল সেকশন */}
+                <div className="w-max flex items-center gap-2 mx-auto text-gray-700 dark:text-white/80">
+                    <img src="https://img.icons8.com/ios-filled/50/mail.png" alt="Mail" className="w-5 dark:invert" />
                     <a href="mailto:contact.arnobbiswas@gmail.com">contact.arnobbiswas@gmail.com</a>
                 </div>
             </div>
@@ -19,26 +27,23 @@ export default function Footer() {
             {/* নিচের বর্ডার এবং কপিরাইট সেকশন */}
             <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
                 
-                {/* আপনার অনুরোধ করা নতুন কপিরাইট টেক্সট */}
-                <p>© 2026 Arnob Biswas • All rights reserved.</p>
+                {/* আপনার নাম এবং কপিরাইট */}
+                <p className="text-gray-600 dark:text-white/70">© 2026 Arnob Biswas • All rights reserved.</p>
 
-                {/* সোশ্যাল মিডিয়া আইকন সেকশন */}
+                {/* সোশ্যাল মিডিয়া আইকন - যা সরাসরি অনলাইন থেকে লোড হবে */}
                 <ul className="flex items-center gap-6 justify-center mt-4 sm:mt-0">
-                    <li>
-                        <a target='_blank' href="#!"><img src="/assets/facebook.png" alt="Facebook" className="w-5 dark:invert" /></a>
-                    </li>
-                    <li>
-                        <a target='_blank' href="#!"><img src="/assets/instagram.png" alt="Instagram" className="w-5 dark:invert" /></a>
-                    </li>
-                    <li>
-                        <a target='_blank' href="#!"><img src="/assets/twitter_x.png" alt="Twitter X" className="w-5 dark:invert" /></a>
-                    </li>
-                    <li>
-                        <a target='_blank' href="#!"><img src="/assets/youtube.png" alt="YouTube" className="w-5 dark:invert" /></a>
-                    </li>
-                    <li>
-                        <a target='_blank' href="#!"><img src="/assets/linkedin.png" alt="LinkedIn" className="w-5 dark:invert" /></a>
-                    </li>
+                    {socialIcons.map((icon, index) => (
+                        <li key={index}>
+                            <a target='_blank' href="#!" className="group">
+                                <img 
+                                    src={icon.url} 
+                                    alt={icon.name} 
+                                    // dark:invert ক্লাসের মাধ্যমে ডার্ক মোডে আইকনগুলো সাদা হয়ে যাবে
+                                    className="w-6 opacity-70 group-hover:opacity-100 group-hover:scale-110 duration-300 dark:invert transition-all" 
+                                />
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
